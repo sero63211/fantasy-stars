756ad4ae056e0328a5c09841a60c8a935ff746c9
maam9 <tohajaman960@gmail.com>
2024-01-05 17:44:57 +0100

    apptsx with routes: detail,edit,create

--- front-end/fantasy-stars/src/App.tsx
+++ front-end/fantasy-stars/src/App.tsx
@@ -1,14 +1,23 @@
-
-import Header from './Components/Header';
-import LoginScreen from './Views/Authentification/LoginScreen';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainScreen from './Views/MainScreen';
+import ItemDetailScreen from './Views/ItemDetailScreen';
+import EditItemDetailScreen from './Views/EditItemDetailScreen';
+import CreateItemScreen from './Views/CreateItemScreen';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainScreen />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<MainScreen />} />
+          <Route path="/details/:id" element={<ItemDetailScreen />} />
+          <Route path="/edit/:id" element={<EditItemDetailScreen />} />
+-          <Route path="/create" element={<CreateItemScreen />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
