0d9e170595e3c97483dbbe640d1d05d02053513d
maam9 <tohajaman960@gmail.com>
2024-01-18 19:08:19 +0100

    header with useauth adjusted

--- front-end/fantasy-stars/src/Components/Header.tsx
+++ front-end/fantasy-stars/src/Components/Header.tsx
@@ -1,37 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../images/fantasy-stars-logo.png';
 import '../Stylings/Header.css';
+import { useAuth } from "../AuthentificationState";
+
 
 const Header: React.FC = () => {
+    const { loggedIn, setLoggedIn } = useAuth();
     const navigate = useNavigate();
-    const [userStatus, setUserStatus] = useState<'login' | 'logout'>('logout');
 
 
     const handleLogoutClick = () => {
-        // Hier kannst du die Logik für den Logout implementieren
-        // Zum Beispiel: Löschen von Authentifizierungstokens
-        // Wenn erfolgreich, setze den Benutzerstatus auf 'logout'
-        setUserStatus('logout');
+        setLoggedIn(false);
     };
 
 
     const handleLoginClick = () => {
-        navigate(`/login`, {
-         
-          });      };
+      navigate(`/login`, {});
+    };
 
     return (
         <header className="header">
             <h1>Fantasy Stars</h1>
             <img src={logo} alt="Fantasy Stars Logo" />
     
-            {userStatus === 'logout' && (
-                <button className="login" onClick={handleLoginClick}>Login</button>
-            )}
-            {userStatus === 'login' && (
-                <button className="logout" onClick={handleLogoutClick}>Logout</button>
-            )}
+            {loggedIn == 
+            false ? 
+              <button className="login" onClick={handleLoginClick}>Login</button>
+            : <button className="logout" onClick={handleLogoutClick}>Logout</button>
+            }
         </header>
     );
 };
